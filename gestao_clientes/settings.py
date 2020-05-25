caf263a83dd37d84046adc7b888a09694485a7d3
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-05-25 16:54:19 -0300

    Criando o template Hello World

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'gestao_clientes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['meus_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
