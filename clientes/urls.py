7467ed5163fb04f28ded3e8926b7111bcf5ff2b7
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-05-25 19:57:38 -0300

    Criando cadastro de cliente

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from clientes.views import persons_list
+from clientes.views import person_new
 
 urlpatterns = [
-    path('list/', persons_list)
+    path('list/', persons_list, name='person_list'),
+    path('new/', person_new, name='person_new'),
 ]
